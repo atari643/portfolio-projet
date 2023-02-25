d36f7759cf7028eac8b0a08219078bfac06e6efb
qartigala <quentin.artigala@etu.u-bordeaux.fr>
2023-02-25 16:31:36 +0100

    ajout de la responsive de l'image

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ portFolio.addEventListener("click", ()=>{
     detail.style.visibility = "hidden"
     portFolio.style.visibility = "hidden"
 })
+
 buttonJava.addEventListener("click", desactiver)
 buttonJava.addEventListener("click", ()=>{
     buttonJava.classList.add("active")
     projet.innerHTML = "<img id='odomo' src='./images/odomo.png' alt='odomo'>"
     var odomo = document.getElementById("odomo")
     odomo.addEventListener("click", ()=>{
-        detail.innerHTML = "<img  src='./images/projet-odomo.png' alt='projet-odomo'>"
-
+        detail.innerHTML = "<img src='./images/projet-odomo.png' alt='projet-odomo'>"
         detail.style.visibility = "visible"
         portFolio.style.visibility = "visible"
     })
@@ -33,7 +33,6 @@ buttonJava.addEventListener("click", ()=>{
 })
 buttonC.addEventListener("click", desactiver)
 buttonC.addEventListener("click", ()=>{
-    buttonC.classList.add("active")
 })
 buttonPython.addEventListener("click", desactiver)
 buttonPython.addEventListener("click", ()=>{
