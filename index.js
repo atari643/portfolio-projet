5320b9734e0737998511dc762f0d4f0836e36954
qartigala <quentin.artigala@etu.u-bordeaux.fr>
2023-03-18 21:24:45 +0100

    Refonte total du code

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var buttonJava = document.getElementById("java")
 var buttonC = document.getElementById("C")
 var buttonHCJ = document.getElementById("HCJ")
 var buttonPython = document.getElementById("Python")
+var buttonLinux = document.getElementById("Linux")
 var buttonMDP = document.getElementById("MDP")
 var buttonBD = document.getElementById("BD")
 var allbutton = document.querySelectorAll("nav div")
-var projet = document.querySelector(".projet")
+var listprojet = document.querySelector(".projet")
 var portFolio = document.querySelector(".portFolio")
 var detail = document.querySelector(".detail")
 var contact = document.getElementById("Contact")
@@ -13,28 +14,94 @@ var ligne = document.querySelector("hr")
 var boutonGithub = document.getElementById("buttonGit")
 var lienGithub = document.querySelector(".Git")
 var infobutton = document.querySelector(".infobutton")
-var countJava = 0;
-
+var sphere = document.querySelector(".sphere")
+ class ProjetJava extends React.Component {
+    render(){
+        const items =[
+            <img class='image' src='./images/odomo.png' alt='odomo'/>,
+            <img class='image' src='./images/tableBiosphère.png' alt='Biosphère'/>,
+            <img class='image' src='./images/odomo.png' alt='odomo'/>,
+            <img class='image' src='./images/odomo.png' alt='odomo'/>
+        ]
+        const java = <img class='logo' src='./images/javalogo.png' alt='javalogo'></img>
+        const lis = items.map(item => <div><li>{java}{item}</li><i class="icofont-close"></i><i class="icofont-expand"></i></div>)
+        return <ul>
+            {lis}
+        </ul>
+    }
+} 
+class ProjetC extends React.Component {
+    render(){
+        const items =[
+        ]
+        const C = <img class='logo' src='./images/javalogo.png' alt='javalogo'/>
+        const lis = items.map(item => <div><li>{C}{item}</li><i class="icofont-close"></i><i class="icofont-expand"></i></div>)
+        return <ul>
+            {lis}
+        </ul>
+    }
+}
 
 
 function desactiver(){
     for(var i = 0; i < allbutton.length; i++){
-        allbutton[i].classList.remove("active");
+
+        allbutton[i].classList.remove("up");
+        allbutton[i].style.setProperty('--h', 100+'%')
+        
     } 
+    listprojet.classList.remove("colorJava");
+    listprojet.classList.remove("colorC")
+    listprojet.classList.remove("colorPy")
     
 }
 
+var etoileHaut = document.querySelector('.Top');
+var etoileBas = document.querySelector('.Bottom');
+var etoileDroite = document.querySelector('.Right');
+var etoileLeft = document.querySelector('.Left');
+let n = 0
+let r = 0;
+let animation = setInterval((e)=>{
+    etoileHaut.style.paddingLeft = n+'%'
+    etoileBas.style.paddingLeft = n+'%'
+    etoileLeft.style.paddingBottom= r+'%'
+    etoileDroite.style.paddingTop = r+'%'
+    if(n<90){
+    n+=0.5;
+    }else{
+        window.clearInterval(animation)
+    }
+    if(r<30){
+        r+=0.17; 
+    }
+    
+}, 16)
+let v = 100
+function up(button){
+    let monter = setInterval(()=>{
+        if(v<200){
+            button.style.setProperty('--h', v+'%')
+        v+=6
+        }else{
+            window.clearInterval(monter)
+        }
+    }, 20)
+}
 
 buttonJava.addEventListener("click", desactiver)
 buttonJava.addEventListener("click", ()=>{
-    countJava+=1;
-    if(countJava==1){
-    projet.classList.toggle("activeJava1")
+    listprojet.classList.add("colorJava")
+    buttonJava.classList.add("up")
+    v=100
+    up(buttonJava)
+    ReactDOM.render(<ProjetJava/>, listprojet)
+    var logo = document .querySelectorAll(".logo")
     setInterval(()=>{
-        projet.classList.toggle("activeJava2")
-    }, 1000)
-    }
-    projet.innerHTML = "<img class='image' src='./images/odomo.png' alt='odomo'>"
+    for(var i=0; i<logo.length; i++){
+        logo[i].classList.toggle("move")
+}}, 2000)
+
     var odomo = document.querySelectorAll(".image")
     for(var i = 0; i < odomo.length; i++){
     odomo[i].addEventListener("click", ()=>{
@@ -62,23 +129,47 @@ infobutton.addEventListener("mouseout", ()=>{
 buttonC.addEventListener("click", desactiver)
 buttonC.addEventListener("click", ()=>{
     buttonC.classList.add("active")
+    listprojet.classList.add("colorC")
+    buttonC.classList.add("up")
+    v=100
+    up(buttonC)
+    ReactDOM.render(<ProjetC/>, listprojet)
 })
 buttonPython.addEventListener("click", desactiver)
 buttonPython.addEventListener("click", ()=>{
     buttonPython.classList.add("active")
+    listprojet.classList.add("colorPy")
+    buttonPython.classList.add("up")
+    v=100
+    up(buttonPython)
 })
 buttonMDP.addEventListener("click", desactiver)
 buttonMDP.addEventListener("click", ()=>{
     buttonMDP.classList.add("active")
+    buttonMDP.classList.add("up")
+    v=100
+    up(buttonMDP)
 })
 buttonBD.addEventListener("click", desactiver)
 buttonBD.addEventListener("click", ()=>{
     buttonBD.classList.add("active")
+    buttonBD.classList.add("up")
+    v=100
+    up(buttonBD)
 })
 buttonHCJ.addEventListener("click", desactiver)
 buttonHCJ.addEventListener("click", ()=>{
     buttonHCJ.classList.add("active")
-    projet.innerHTML = "<img id='odomo' src='./images/odomo.png' alt='odomo'><img id='odomo' src='./images/odomo.png' alt='odomo'><img id='odomo' src='./images/odomo.png' alt='odomo'><img id='odomo' src='./images/odomo.png' alt='odomo'><img id='odomo' src='./images/odomo.png' alt='odomo'><img id='odomo' src='./images/odomo.png' alt='odomo'><img id='odomo' src='./images/odomo.png' alt='odomo'>"
+    buttonHCJ.classList.add("up")
+    v=100
+    up(buttonHCJ)
+})
+buttonLinux.addEventListener("click", desactiver)
+buttonLinux.addEventListener("click", ()=>{
+    buttonLinux.classList.add("active")
+    buttonLinux.classList.add("up")
+    v=100
+    up(buttonLinux)
 })
 contact.addEventListener("mouseover", ()=>{
     ligne.style.visibility = "visible"
@@ -91,6 +182,3 @@ contact.addEventListener("mouseout", ()=>{
     ligne.classList.remove("souligner")
         
 })
-if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
-    console.log('🎉 Dark mode is supported');
-  }
