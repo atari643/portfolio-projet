bc48154da46561c3302a1a30b53c2af1a963d453
qartigala <quentin.artigala@etu.u-bordeaux.fr>
2023-03-25 16:55:40 +0100

    update

--- index.js
+++ index.js
@@ -11,32 +11,38 @@ var Portfolio = document.querySelector(".portFolio")
 var contact = document.getElementById("Contact")
 var sphere = document.querySelector(".sphere")
 var all = document.querySelector(".page")
+var afficher = document.querySelector("#afficher")
 
+class Description extends React.Component{
+    render(){
+        return <div id="Description">
+            <p>Passionner par l'informatique à l'âge de 10 ans, il cherche un moyen d'en apprendre toujours plus dans tous les langages.</p>
+        </div>
+    }
+}
 class ProjetJava extends React.Component {
     render() {
         const items = [
             <img class='image odomo' src='./images/odomo.png' alt='odomo' />,
             <img class='image biosphère' src='./images/tableBiosphère.png' alt='Biosphère' />,
         ]
-
         const java = <img class='logo' src='./images/javalogo.png' alt='javalogo'></img>
-        const lis = items.map(item => <div><li>{java}{item}</li><i class="icofont-close"></i><i class="icofont-expand"></i></div>)
+        const lis = items.map(item => <div><li>{java}{item}</li><i class="icofont-expand icofont-2x"></i></div>)
         return <ul>
             {lis}
         </ul>
     }
 }
-class RetextJava extends React.Component{
-    render(){
-        const items = [
-            <img id='retexOdomo' src='./images/projet-odomo.png' alt='projet'></img>
-        ]
-        const close = <div class="cercle close"><i class="icofont-close-circled icofont-5x"></i></div>
-        const lis = items.map(item => <React.Fragment>{item}{close}</React.Fragment>)
-        return <div class='detail'>
-            {lis}
+class FenetreProjet extends React.Component {
+    render() {
+        return <div class="contenu">
+            <i class="icofont-close-line-circled icofont-5x"></i>
+            <h1>{this.props.title}</h1>
+            <img class='big' src={this.props.image}></img>
+            <p>{this.props.text}</p>
+            <button class="Download"><i class="icofont-download"></i>Download</button>
         </div>
-    }
+}
 }
 class ProjetC extends React.Component {
     render() {
@@ -50,7 +56,10 @@ class ProjetC extends React.Component {
     }
 }
 
-
+afficher.addEventListener('click',()=>{
+    ReactDOM.render(<Description/>, document.querySelector("#me"));
+    document.querySelector("#me").classList.toggle("down");
+})
 function desactiver() {
     for (var i = 0; i < allbutton.length; i++) {
 
@@ -85,24 +94,10 @@ let animation = setInterval((e) => {
     }
 
 }, 16)
-let v = 100
-function up(button) {
-    let monter = setInterval(() => {
-        if (v < 200) {
-            button.style.setProperty('--h', v + '%')
-            v += 6
-        } else {
-            window.clearInterval(monter)
-        }
-    }, 20)
-}
-
 buttonJava.addEventListener("click", desactiver)
 buttonJava.addEventListener("click", () => {
     listprojet.classList.add("colorJava")
     buttonJava.classList.add("up")
-    v = 100
-    up(buttonJava)
     ReactDOM.render(<ProjetJava />, listprojet)
     var logo = document.querySelectorAll(".logo")
     setInterval(() => {
@@ -114,29 +109,22 @@ buttonJava.addEventListener("click", () => {
     var open = document.querySelectorAll(".icofont-expand")
     open.forEach((userItem) => {
     userItem.addEventListener("click",()=>{
-        userItem.parentElement.style.position="fixed"
-        userItem.parentElement.style.width = "75%"
-        userItem.parentElement.style.border = "solid red"
-        userItem.parentElement.style.top = "0"
-        userItem.parentElement.style.zIndex = "2"
-        userItem.parentElement.style.backgroundColor = "black"
+        console.log(userItem.parentElement.firstChild.lastChild.src)
+        if(userItem.parentElement.firstChild.lastChild.src=="http://127.0.0.1:5500/images/odomo.png"){
+        ReactDOM.render(<FenetreProjet image={"./images/projet-odomo.png"} text="Dans le projet odomo une grande difficulté c’était le sens de lecture dès valeur dans un tableau ou une matrice. La façon donc les valeurs sont agencés n’est pas forcément comment on aimerait les afficher. J’ai réussi à faire que pour chaque histogramme selon son ordre de lecture, les valeurs se rangent dans l’ordre attendu.
+        La lecture un tableau/matrice était essentiel dans la réalisation de ce projet" title="Projet météorologique"></FenetreProjet>, document.querySelector(".window"))
+        }else if(userItem.parentElement.firstChild.lastChild.src=="http://127.0.0.1:5500/images/tableBiosph%C3%A8re.png"){
+            ReactDOM.render(<FenetreProjet image={"./images/projet-Biosphère.png"} text="Le projet se nomme Biosphère7 qui est écrit en java. C’est un jeu de plateau à 15 niveaux  qui permet d’effectuer différentes actions selon les règles du niveau qui se cumule. Ma contribution est la réalisation,
+            jusqu’au niveau 11, du tableau des actions possibles selon les nouvelles règles de chaque niveau. La deuxième partie du projet a été pour moi la plus enrichissante avec l’implémentation d’une IA avec le choix de la méthode à suivre, 
+            et j’ai réussi à faire une méthode d’apprentissage par renforcement avec un algorithme QLearning (off policy). " title="Projet conception d'un jeu et une IA"></FenetreProjet>, document.querySelector(".window"))
+        }
+        document.querySelector(".window").classList.remove("cacher")
+        document.querySelector(".icofont-close-line-circled").addEventListener("click",()=>{
+            ReactDOM.render(<FenetreProjet image={""} text="" title=""></FenetreProjet>, document.querySelector(".window"))
+            document.querySelector(".window").classList.add("cacher")
+        })
     })
 })
-    odomo.addEventListener("click", ()=>{
-        ReactDOM.render(<RetextJava />, Portfolio)
-        var odomoProjet = document.querySelector("#retexOdomo")
-        all.style.filter="brightness(50%)"
-        odomoProjet.style.filter="brightness(100%)"
-        odomoProjet.style.display="block"
-        document.querySelector(".cercle").style.display="block"
-        document.querySelector(".cercle").addEventListener("click", ()=>{
-            odomoProjet.style.display="none"
-            all.style.filter="brightness(100%)"
-            document.querySelector(".cercle").style.display="none"
-        })
-        
-    })
-    
 
     }
 )
@@ -146,7 +134,6 @@ buttonC.addEventListener("click", () => {
     listprojet.classList.add("colorC")
     buttonC.classList.add("up")
     v = 100
-    up(buttonC)
     ReactDOM.render(<ProjetC />, listprojet)
 })
 buttonPython.addEventListener("click", desactiver)
